caed760d0c6ab87f8cafc5d1b55f2ac5dc243467
Squill <williamfeeney123@gmail.com>
2019-10-29 00:42:36 -0400

    AARON
    WILL

---
+++ Assets/Scenes/Moving.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask PlatformsLayerMask;
+    public float speed;
+
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.03f;
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movePosition = new Vector2(transform.position.x, transform.position.y);
+        transform.position = movePosition;
+
+
+      if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector2(movePosition.x + speed, movePosition.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector2(movePosition.x - speed, movePosition.y);
+        }
+
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+
+
+    }
+    private bool  IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down * .1f, PlatformsLayerMask);
+    
+        return raycastHit2d.collider != null;
+        }
+
+}
---
+++ Assets/Scenes/collisionTester.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Make sure to add this for access to the SceneManagment class.
+
+
+public class collisionTester : MonoBehaviour
+{
+    public GameObject spikes;
+    public GameObject spike;
+    public GameObject trap;
+    public GameObject spikeTrap;
+    
+
+
+
+    //Any Collider2D component will call this function on
+    //any attached scripts when the collider enters a collision with another collider.
+    //The gameobject must also have a RigidBody2D attached.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+
+        if (collision.collider.gameObject == spikes)
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (collision.collider.gameObject == spike)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (collision.collider.gameObject == trap)
+        {
+            GameObject newBall = Instantiate(spikeTrap, new Vector3(4, 10, 0f), Quaternion.identity);
+          
+        }
+        if (collision.collider.gameObject == spikeTrap)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
+
+
